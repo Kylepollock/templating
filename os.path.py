be46fef1ef27d5733c3cc8ae56e6fe6c332e3f35
Kyle Pollock <polloc21@gmail.com>
2019-07-03 23:39:41 -0700

    edits

--- os.path.py
+++ os.path.py
@@ -4,3 +4,10 @@ file_name = os.path.basename(file_path)
 print(file_name)
 name_only, extension = os.path.splitext(file_name)
 print(name_only)
+
+
+pages = [] pages.append({
+    "filename": "content/index.html",
+    "title": "Index",
+    "output": "docs/index.html",
+}) print(pages)
