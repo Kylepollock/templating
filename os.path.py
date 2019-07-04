9810ef496906bcbcc20c1b49ec182a0beb2c01f1
Kyle Pollock <polloc21@gmail.com>
2019-07-04 09:53:41 -0700

    all pages

--- os.path.py
+++ os.path.py
@@ -5,9 +5,25 @@ print(file_name)
 name_only, extension = os.path.splitext(file_name)
 print(name_only)
 
+file_path = "content/contact.html"
+file_name = os.path.basename(file_path)
+print(file_name)
+name_only, extension = os.path.splitext(file_name)
+print(name_only)
 
-pages = [] pages.append({
-    "filename": "content/index.html",
-    "title": "Index",
-    "output": "docs/index.html",
-}) print(pages)
+file_path = "content/index.html"
+file_name = os.path.basename(file_path)
+print(file_name)
+name_only, extension = os.path.splitext(file_name)
+print(name_only)
+
+
+# def write_pages():
+
+# 	pages = [] pages.append({
+# 	    "filename": "content/index.html",
+# 	    "title": "Index",
+# 	    "output": "docs/index.html",
+# 	}) print(pages)
+
+# write_pages()
