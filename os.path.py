9e4e1192c1d53d93871b9a2d0298ebf0234ded72
Kyle Pollock <polloc21@gmail.com>
2019-07-04 22:27:56 -0700

    add all pages

--- os.path.py
+++ os.path.py
@@ -1,3 +1,5 @@
+print('---this is the os path file!---')
+
 import os
 file_path = "content/give.html"
 file_name = os.path.basename(file_path)
@@ -18,12 +20,23 @@ name_only, extension = os.path.splitext(file_name)
 print(name_only)
 
 
-# def write_pages():
+def write_pages():
 
-# 	pages = [] pages.append({
-# 	    "filename": "content/index.html",
-# 	    "title": "Index",
-# 	    "output": "docs/index.html",
-# 	}) print(pages)
+	pages = [] 
+	pages.append({
+	    "filename": "content/index.html",
+	    "title": "Index",
+	    "output": "docs/index.html",
 
-# write_pages()
+	    "filename": "content/contact.html",
+	    "title": "Contact",
+	    "output": "docs/contact.html",
+
+
+	    "filename": "content/give.html",
+	    "title": "Give",
+	    "output": "docs/give.html",
+	}) 
+	print(pages)
+
+write_pages()
